ade5671623252345f3cc4c08bceb1af2f49fc4e8
maxi <maxinuss@gmail.com>
2020-02-25 21:41:37 -0300

    formatted data

--- routes.js
+++ routes.js
@@ -1,9 +1,6 @@
 const bodyParser = require('body-parser');
 const googlePlace = require('./src/models/GooglePlace');
 
-const GOOGLE_MAPS_SEARCH_DEFAULT_KEYWORD = process.env.GOOGLE_MAPS_SEARCH_DEFAULT_KEYWORD;
-const GOOGLE_MAPS_SEARCH_DEFAULT_TYPE = process.env.GOOGLE_MAPS_SEARCH_DEFAULT_TYPE;
-
 function init(app, router) {
   app.use(bodyParser.json());
   routes(router);
@@ -23,10 +20,8 @@ function routes(router) {
 
   router.get('/properties', async (req, res) => {
     const latLong = req.query.at;
-    const result = await googlePlace.getPlace(
-        latLong,
-        GOOGLE_MAPS_SEARCH_DEFAULT_KEYWORD,
-        GOOGLE_MAPS_SEARCH_DEFAULT_TYPE
+    const result = await googlePlace.getFormattedPlace(
+        latLong
     );
 
     res.status(200).json(result);
--- src/models/GooglePlace.js
+++ src/models/GooglePlace.js
@@ -4,20 +4,39 @@ const client = new Client({});
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 const GOOGLE_MAPS_SEARCH_DEFAULT_TIMEOUT = process.env.GOOGLE_MAPS_SEARCH_DEFAULT_TIMEOUT;
 const GOOGLE_MAPS_SEARCH_DEFAULT_RADIUS = process.env.GOOGLE_MAPS_SEARCH_DEFAULT_RADIUS;
+const GOOGLE_MAPS_SEARCH_DEFAULT_KEYWORD = process.env.GOOGLE_MAPS_SEARCH_DEFAULT_KEYWORD;
+const GOOGLE_MAPS_SEARCH_DEFAULT_TYPE = process.env.GOOGLE_MAPS_SEARCH_DEFAULT_TYPE;
 
-async function getPlace(latLong, keyword, type){
+async function getPlace(latLong){
     const result = await client.placesNearby({
             params: {
                 key: GOOGLE_MAPS_API_KEY,
                 location: latLong,
-                keyword: keyword,
-                type: type,
-                radius: GOOGLE_MAPS_SEARCH_DEFAULT_RADIUS,
+                keyword: GOOGLE_MAPS_SEARCH_DEFAULT_KEYWORD,
+                type: GOOGLE_MAPS_SEARCH_DEFAULT_TYPE,
+                radius: parseInt(GOOGLE_MAPS_SEARCH_DEFAULT_RADIUS),
             },
-            timeout: GOOGLE_MAPS_SEARCH_DEFAULT_TIMEOUT
+            timeout: parseInt(GOOGLE_MAPS_SEARCH_DEFAULT_TIMEOUT)
         });
 
     return result.data.results;
 }
 
-module.exports.getPlace = getPlace;
+async function getFormattedPlace(latLong){
+    const data = await getPlace(latLong);
+    let results = [];
+
+    for (let hotel of data) {
+        results.push({
+            id: hotel.place_id,
+            name: hotel.name,
+            rating: hotel.rating,
+            vicinity: hotel.vicinity,
+            latLong: `${hotel.geometry.location.lat}, ${hotel.geometry.location.lng}`
+        })
+    }
+
+    return results;
+}
+
+module.exports.getFormattedPlace = getFormattedPlace;
